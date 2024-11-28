d59c28e7b877d0a43d6b23ec17748787e25d904a
Lucas Henrique <lucas.henrique0023@gmail.com>
2024-11-28 18:32:18 -0300

    Dois erros no mesmo teste para Product

--- clean_architecture_desafio_notification/src/domain/product/entity/product.spec.ts
+++ clean_architecture_desafio_notification/src/domain/product/entity/product.spec.ts
@@ -13,6 +13,12 @@ describe("Product unit tests", () => {
     }).toThrowError("product: Name is required");
   });
 
+  it("should throw error when name is empty and price is zero", () => {
+    expect(() => {
+      const product = new Product("123", "", 0);
+    }).toThrowError("product: Name is required,product: Price must be greater than zero");
+  });
+
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
